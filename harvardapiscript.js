cea0cb06bb14670083adfade245694462136f1df
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-06-28 13:52:34 -0400

    it now pulls all the relevant information about the image

--- harvardapiscript.js
+++ harvardapiscript.js
@@ -1,4 +1,4 @@
-let keyword = "real";
+let keyword = "education sarcasm control";
 
 let queryURL =
   "https://api.harvardartmuseums.org/object?q=keyword=" +
@@ -10,9 +10,37 @@ $.ajax({
   method: "GET",
 }).then(function (response) {
   console.log(response);
-  let imageUrl = response.records[0].primaryimageurl;
+  let thisItem = response.records[0];
+  //provides the image url to show the image
+  let imageUrl = thisItem.primaryimageurl;
   console.log(imageUrl);
   let image = $("<img>");
   image.attr("src", imageUrl);
   $("body").append(image);
+  // pulls the title of the piece
+  let title = thisItem.title;
+  console.log(title);
+  //pulls the medium of the piece
+  let medium = thisItem.medium;
+  console.log(medium);
+  //gets the artist's name
+  let artist = thisItem.people[0].name;
+  console.log(artist);
+  //provides three options for how to pull the date, depending on what is provided in the api
+  let date;
+  let period = thisItem.period;
+  //the first way will get the exact year it was made
+  if (thisItem.dated !== null) {
+    date = thisItem.dated;
+    console.log(title + " was made in " + date);
+  }
+  //the second will provide the century it was made in
+  else if (thisItem.century !== null) {
+    date = thisItem.century;
+    console.log(title + " was made in the " + date);
+  }
+  //if neither the date or century is available, it'll get the general period it was created in
+  else {
+    console.log(title + " was made in the " + period);
+  }
 });

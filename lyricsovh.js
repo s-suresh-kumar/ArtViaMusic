cd501bef9b8f52c0d59f83863e83b72270368b8b
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-06-29 20:15:18 -0400

    adjusted the javascript to jquery for consistency.

--- lyricsovh.js
+++ lyricsovh.js
@@ -1,6 +1,21 @@
-function findlyrics(){
-    $.get("https://api.lyrics.ovh/v1/"+document.getElementById("artist").value+"/"+document.getElementById("title").value,
-        function(data){
-        document.getElementById("output").innerHTML=data.lyrics.replace(new RegExp("/n","g"),"<br>")
-    })
-}  
+// function findlyrics(){
+//     $.get("https://api.lyrics.ovh/v1/"+document.getElementById("artist").value+"/"+document.getElementById("title").value,
+//         function(data){
+//         document.getElementById("output").innerHTML=data.lyrics.replace(new RegExp("/n","g"),"<br>")
+//     })
+// }
+function findlyrics() {
+  let title = $("#title").val();
+  let artist = $("#artist").val();
+
+  console.log(title, artist);
+
+  let queryURL = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
